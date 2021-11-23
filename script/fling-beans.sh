474e8b56371808d3615949e83f6b4203e6a06b2f
Rob Thijssen <rthijssen@gmail.com>
2021-11-23 17:45:04 +0200

    auto gleaned beans

---
+++ script/fling-beans.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true; do
+  cd /run/media/grenade/953af29f-6eba-461e-8c01-eae014e770e9/beans/data
+  script/stir-beans.sh
+  cd /run/media/grenade/953af29f-6eba-461e-8c01-eae014e770e9/beans
+  git add .
+  git commit -m "auto gleaned beans"
+  git push origin main
+  sleep 1200
+done
