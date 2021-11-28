105ef3defde836ff92ddd79808bbe880cb69f7e4
Rob Thijssen <rthijssen@gmail.com>
2021-11-28 12:52:21 +0200

    auto gleaned beans

--- script/bake-beans.sh
+++ script/bake-beans.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-mkdir ./task
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+data_dir=${script_dir}/../data
+task_dir=${data_dir}/task
+mkdir -p ${task_dir}
 
 while true; do
   ye=2021
@@ -9,28 +12,26 @@ while true; do
       if date -d ${dt}; then
         for hr in ${dt}-{00..23}; do
           echo "- ${hr}"
-          if [ -s ${mo}/${dt}/${hr}.csv ]; then
-            for task_id in $(cat ${mo}/${dt}/${hr}.csv | cut -d ',' -f3); do
+          if [ -s ${data_dir}/${mo}/${dt}/${hr}.csv ]; then
+            for task_id in $(cat ${data_dir}/${mo}/${dt}/${hr}.csv | cut -d ',' -f3); do
               echo "  - ${task_id}"
-              if [ ! -s ./task/${task_id}.json ]; then
-                if curl -sL -o ./task/${task_id}.json https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/${task_id}; then
-                  echo "    - ./task/${task_id}.json fetched from https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/${task_id}"
-                elif [ -f ./task/${task_id}.json ]; then
-                  rm ./task/${task_id}.json
-                  echo "    - failed to fetch ./task/${task_id}.json from https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/${task_id}"
+              if [ ! -s ${task_dir}/${task_id}.json ]; then
+                if curl -sL -o ${task_dir}/${task_id}.json https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/${task_id}; then
+                  echo "    - ${task_dir}/${task_id}.json fetched from https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/${task_id}"
+                elif [ -f ${task_dir}/${task_id}.json ]; then
+                  rm ${task_dir}/${task_id}.json
+                  echo "    - failed to fetch ${task_dir}/${task_id}.json from https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/${task_id}"
                 else
-                  echo "    - failed to fetch ./task/${task_id}.json from https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/${task_id}"
+                  echo "    - failed to fetch ${task_dir}/${task_id}.json from https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/${task_id}"
                 fi
-              else
-                echo "    - ./task/${task_id}.json detected"
               fi
-              if [ -s ./task/${task_id}.json ]; then
-                queue=$(jq -r '.taskQueueId' ./task/${task_id}.json)
-                name=$(jq -r '.metadata.name' ./task/${task_id}.json)
-                if [ "${queue}" != "null" ] && ! grep -q ${task_id} task-${hr}.csv; then
-                  echo ${dt},${hr##*-},${queue},${task_id},${name} >> task-${mo}.csv 
-                  echo ${dt},${hr##*-},${queue},${task_id},${name} >> task-${dt}.csv
-                  echo ${dt},${hr##*-},${queue},${task_id},${name} >> task-${hr}.csv
+              if [ -s ${task_dir}/${task_id}.json ]; then
+                queue=$(jq -r '.taskQueueId' ${task_dir}/${task_id}.json)
+                name=$(jq -r '.metadata.name' ${task_dir}/${task_id}.json)
+                if [ "${queue}" != "null" ] && ! grep -q ${task_id} ${data_dir}/task-${hr}.csv; then
+                  echo ${dt},${hr##*-},${queue},${task_id},${name} >> ${data_dir}/task-${mo}.csv 
+                  echo ${dt},${hr##*-},${queue},${task_id},${name} >> ${data_dir}/task-${dt}.csv
+                  echo ${dt},${hr##*-},${queue},${task_id},${name} >> ${data_dir}/task-${hr}.csv
                 fi
               fi
             done
@@ -49,7 +50,7 @@ while true; do
                     }
                   }
                ]
-            }' task-${hr}.csv > task-${hr}.json
+            }' ${data_dir}/task-${hr}.csv > ${data_dir}/task-${hr}.json
           fi
         done
         if [ -s task-${dt}.csv ]; then
@@ -68,7 +69,7 @@ while true; do
                   }
                 }
              ]
-          }' task-${dt}.csv > task-${dt}.json
+          }' ${data_dir}/task-${dt}.csv > ${data_dir}/task-${dt}.json
         fi
       fi
     done
@@ -88,7 +89,7 @@ while true; do
               }
             }
          ]
-      }' task-${mo}.csv > task-${mo}.json
+      }' ${data_dir}/task-${mo}.csv > ${data_dir}/task-${mo}.json
     fi
   done
 done
