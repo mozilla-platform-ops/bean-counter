6dbc97dc4dbe6a448f48a158590874598d7b7368
Rob Thijssen <rthijssen@gmail.com>
2021-11-28 12:31:45 +0200

    auto gleaned beans

--- script/glean-beans.sh
+++ script/glean-beans.sh
@@ -1,44 +1,48 @@
 #!/bin/bash
 
+papertrail_token=$(pass Mozilla/papertrail/grenade-token)
+
 local_archive=/home/grenade/pt-logs/pt-logs
 
 ye=2021
 #for mo in ${ye}-{01..12}; do
-for mo in ${ye}-{11,09,08}; do
+for mo in ${ye}-{08..10}; do
   for dt in ${mo}-{01..31}; do
-    echo "- ${dt}"
-    mkdir -p ${mo}/${dt}
-    for archive in ./${dt}-{00..23}.tsv.gz; do
-      bname=$(basename ${archive})
-      dthr=${bname%%.*}
-      #if [ -s ${mo}/${dt}/${dthr}.csv ] && grep -Fxq "${dt},${dthr##*-}," ${mo}/${dt}/${dthr}.csv; then
-      if [ -s ${mo}/${dt}/${dthr}.csv ]; then
-        echo "  - ${dthr}: detected previous processing"
-      else
-        echo "  - ${dthr}: processing..."
-        if [ ! -f ${archive} ] && [ -s ${local_archive}/${bname} ] && gzip -t ${local_archive}/${bname}; then
-          mv ${local_archive}/${bname} ${archive}
-        fi
-        if [ ! -s ${archive} ] || ! gzip -t ${archive}; then
-          url=https://papertrailapp.com/api/v1/archives/${dthr}/download
-          if curl \
-            -H "X-Papertrail-Token: $(pass Mozilla/papertrail/grenade-token)" \
-            -o ${archive} \
-            -L ${url} && gzip -t ${archive}; then
-            echo "    - ${archive} fetched from ${url}"
-          else
-            echo "    - failed to fetch ${archive} from ${url}"
-            [ -f ${archive} ] && rm ${archive}
-          fi
+    if date -d ${dt}; then
+      echo "- ${dt}"
+      mkdir -p ${mo}/${dt}
+      for archive in ./${dt}-{00..23}.tsv.gz; do
+        bname=$(basename ${archive})
+        dthr=${bname%%.*}
+        #if [ -s ${mo}/${dt}/${dthr}.csv ] && grep -Fxq "${dt},${dthr##*-}," ${mo}/${dt}/${dthr}.csv; then
+        if [ -s ${mo}/${dt}/${dthr}.csv ]; then
+          echo "  - ${dthr}: detected previous processing"
         else
-          echo "    - ${archive} detected locally"
+          echo "  - ${dthr}: processing..."
+          if [ ! -f ${archive} ] && [ -s ${local_archive}/${bname} ] && gzip -t ${local_archive}/${bname}; then
+            mv ${local_archive}/${bname} ${archive}
+          fi
+          if [ ! -s ${archive} ] || ! gzip -t ${archive}; then
+            url=https://papertrailapp.com/api/v1/archives/${dthr}/download
+            if curl \
+              -H "X-Papertrail-Token: ${papertrail_token}" \
+              -o ${archive} \
+              -L ${url} && gzip -t ${archive}; then
+              echo "    - ${archive} fetched from ${url}"
+            else
+              echo "    - failed to fetch ${archive} from ${url}"
+              [ -f ${archive} ] && rm ${archive}
+            fi
+          else
+            echo "    - ${archive} detected locally"
+          fi
+          if [ -s ${archive} ] && gzip -t ${archive}; then
+            gzip -cd ${archive} | grep "finished successfully" | cut -f5,10 | sed -n 's/^\([^\t]*\)\t.* \([^\s]*\) finished successfully.*$/'${dt},${dthr##*-}',\2,\1/p' >> ${mo}/${dt}/${dthr}.csv
+          fi
         fi
-        if [ -s ${archive} ] && gzip -t ${archive}; then
-          gzip -cd ${archive} | grep "finished successfully" | cut -f5,10 | sed -n 's/^\([^\t]*\)\t.* \([^\s]*\) finished successfully.*$/'${dt},${dthr##*-}',\2,\1/p' >> ${mo}/${dt}/${dthr}.csv
-        fi
-      fi
-    done
-    sort -u ${mo}/${dt}/${dt}-*.csv > ${mo}/${dt}.csv
+      done
+      sort -u ${mo}/${dt}/${dt}-*.csv > ${mo}/${dt}.csv
+    fi
   done
   sort -u ${mo}/*.csv > ${mo}.csv
 done
