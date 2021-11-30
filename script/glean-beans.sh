1381123dc84608f4adb4b9b129ae11d31eae5967
Rob Thijssen <rthijssen@gmail.com>
2021-11-30 16:24:39 +0200

    pm2: bake beans

--- script/glean-beans.sh
+++ script/glean-beans.sh
@@ -9,14 +9,15 @@ for ye in 2021; do
     for dt in ${mo}-{01..31}; do
       if date -d ${dt} &> /dev/null; then
         echo "- ${dt}"
-        mkdir -p ${mo}/${dt}
+        mkdir -p ${data_dir}/${mo}/${dt}
         for dthr in ${dt}-{00..23}; do
           archive=${data_dir}/${dthr}.tsv.gz
-          #if [ -s ${mo}/${dt}/${dthr}.csv ] && grep -Fxq "${dt},${dthr##*-}," ${mo}/${dt}/${dthr}.csv; then
-          if [ -s ${mo}/${dt}/${dthr}.csv ]; then
-            echo "  - ${dthr}: detected previous processing"
+          csv_hr=${data_dir}/${mo}/${dt}/${dthr}.csv
+          #if [ -s ${csv_hr} ] && grep -Fxq "${dt},${dthr##*-}," ${csv_hr}; then
+          if [ -s ${csv_hr} ]; then
+            echo "  - ${dthr}: detected previous processing (${csv_hr})"
           else
-            echo "  - ${dthr}: processing..."
+            echo "  - ${dthr}: processing (creating ${csv_hr})..."
             if [ ! -s ${archive} ] || ! gzip -t ${archive}; then
               url=https://papertrailapp.com/api/v1/archives/${dthr}/download
               if curl \
@@ -32,13 +33,14 @@ for ye in 2021; do
               echo "    - ${archive} detected locally"
             fi
             if [ -s ${archive} ] && gzip -t ${archive}; then
-              gzip -cd ${archive} | grep "finished successfully" | cut -f5,10 | sed -n 's/^\([^\t]*\)\t.* \([^\s]*\) finished successfully.*$/'${dt},${dthr##*-}',\2,\1/p' >> ${mo}/${dt}/${dthr}.csv
+              gzip -cd ${archive} | grep "finished successfully" | cut -f5,10 | sed -n 's/^\([^\t]*\)\t.* \([^\s]*\) finished successfully.*$/'${dt},${dthr##*-}',\2,\1/p' >> ${csv_hr}
+              echo "    - extracted $(wc -l < ${csv_hr}) tasks from ${dthr}.tsv.gz to ${csv_hr}"
             fi
           fi
         done
-        sort -u ${mo}/${dt}/${dt}-*.csv > ${mo}/${dt}.csv
+        sort -u ${data_dir}/${mo}/${dt}/${dt}-*.csv > ${data_dir}/${mo}/${dt}.csv
       fi
     done
-    sort -u ${mo}/*.csv > ${mo}.csv
+    sort -u ${data_dir}/${mo}/*.csv > ${data_dir}/${mo}.csv
   done
 done
